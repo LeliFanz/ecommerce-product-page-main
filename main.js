c42420f50f8d84307e7a42e3bc2568c382748ff5
LeliFanz <lelifanz@gmail.com>
2022-10-28 20:33:07 -0300

    Solo falta el modal img

--- main.js
+++ main.js
@@ -2,6 +2,9 @@
 let imagenGrande = document.querySelector(".card-img-top");
 let imagenChica = document.querySelector(".image");
 let imagencita = document.querySelectorAll(".col-3");
+let slide_btn = document.querySelector(".slide_btn");
+let previous_btn = document.querySelector(".previous_btn");
+let next_btn = document.querySelector(".next_btn");
 // agregando a la cesta
 let botonMenos = document.getElementById("button-addon1");
 let botonMas = document.getElementById("button-addon2");
@@ -13,7 +16,6 @@ let contador = 0;
 let cart = document.getElementById("cart"); //carrito nav
 let carrito = document.querySelector(".carrito"); //carrito content
 
-let imagen = document.querySelector(".card-img-top");
 let producto = document.querySelector("#producto");
 let precio = document.querySelector("span.precio");
 let template = document.getElementById("template-li");
@@ -29,17 +31,35 @@ let cart_subtotal = document.querySelector(".cart_subtotal");
 let cart_delete = document.querySelector(".cart_delete");
 let cart_checkout = document.querySelector(".cart_checkout");
 
-// modificando las imagenes
+// Modificando Imagenes
+let imagenDefault = document.querySelector(".active");
+imagenGrande.src = imagenDefault.src;
+let i = imagenDefault.id;
+// modificando las imagenes pouch
 imagenChica.addEventListener("click", (e) => {
-  imagenGrande.src = e.target.src;
-  for (i = 0; i < imagencita.length; i++) {
+  for (let i = 0; i < imagencita.length; i++){
+    imagencita[i].classList.remove("active");
     imagencita[i].style.opacity = "1";
   }
+  e.target.classList.add("active");
+  imagenGrande.src = e.target.src;
   e.target.style.opacity = "50%";
+  i = e.target.id
+});
+// modificando las imagenes slider
+slide_btn.addEventListener("click", (e) => {
+  if (e.target == previous_btn) {
+    cambiarImagenPrevious(e);
+    limpiarStyle();
+  }
+  if (e.target == next_btn) {
+    cambiarImagenNext(e);
+    limpiarStyle();
+  }
 });
 // insertar al carrito
 botonAddToCar.addEventListener("click", () => {
-  cart_img.src = imagen.src;
+  cart_img.src = imagenGrande.src;
   cart_producto.textContent = producto.textContent;
   cart_precio.textContent = precio.textContent;
   if (cart_cantidad.textContent == 0) {
@@ -68,6 +88,7 @@ botonAddToCar.addEventListener("click", () => {
     cart_badge.textContent = cart_cantidad.textContent;
     cart_vacio.style.display = "none";
     cart_compra.style.display = "flex";
+    cart_checkout.style.display = "block";
   }
 });
 // delete de la cesta
@@ -95,3 +116,33 @@ cart.addEventListener("click", () => {
     cart_vacio.style.display = "block";
   }
 });
+// FUNCIONES
+// modificando las imagenes slider
+function limpiarStyle() {
+  for (let i = 0; i < imagencita.length; i++) {
+    imagencita[i].style.opacity = "1";
+  }
+  imagencita[i].style.opacity = "50%";
+}
+function cambiarImagenPrevious(e) {
+  e.preventDefault();
+  imagencita[i].classList.remove("active");
+  i--
+  if (i < 0) {
+    i = imagencita.length - 1;
+  }
+  imagencita[i].classList.add("active");
+  let imagenDefault = document.querySelector(".active");
+  imagenGrande.src = imagenDefault.src;
+}
+function cambiarImagenNext(e) {
+    e.preventDefault();
+    imagencita[i].classList.remove("active");
+    i++
+    if (i == imagencita.length) {
+      i = 0
+    }
+    imagencita[i].classList.add("active");
+    let imagenDefault = document.querySelector(".active");
+    imagenGrande.src = imagenDefault.src;
+  }
